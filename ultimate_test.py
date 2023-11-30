c7ef25d36bed3773bb737b3b84b49e7c67906d88
Joaber Narciso <42392392+joabernarc@users.noreply.github.com>
2023-11-30 14:28:09 +0900

    Update ultimate_test.py
    Now able to handle many users at the same time

--- ultimate_test.py
+++ ultimate_test.py
@@ -1,21 +1,32 @@
 import os
-
+import warnings
 import requests
 import json
 import pandas as pd
 
+from pandas import DataFrame
 from flask import Flask, request, abort
+
+from linebot import LineBotSdkDeprecatedIn30
+
 from linebot import (
-    LineBotApi, WebhookHandler
+    LineBotApi
 )
 from linebot.v3.messaging import (
     Configuration
+)
+from linebot.v3.webhook import (
+    WebhookHandler
+)
+from linebot.v3.webhooks import (
+    MessageEvent,
+    TextMessageContent
 )
 from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TemplateSendMessage, CarouselTemplate, CarouselColumn)
+    TextMessage, TemplateSendMessage, CarouselTemplate, CarouselColumn)
 
 app = Flask(__name__)
 
@@ -25,6 +36,8 @@ line_bot_api = LineBotApi(access_token)
 handler = WebhookHandler('3879d5592c6cff489b2ce1540972e4de')
 res_rakuten = requests.get('https://app.rakuten.co.jp/services/api/Recipe/CategoryList/20170426?applicationId=1043289948570835263')
 rakuten_data = json.loads(res_rakuten.text)
+
+user_states = {}
 df = pd.DataFrame(columns=['category1','category2','category3','categoryId','categoryName'])
 df_keyword = pd.DataFrame()
 last_category_search = ''
@@ -62,58 +75,41 @@ def callback():
     return 'OK'
 
 
-@handler.add(MessageEvent, message=TextMessage)
+@handler.add(MessageEvent, message=TextMessageContent)
 def response_message(event):
     # notesのCarouselColumnの各値は、変更してもらって結構です。
-    global df, df_keyword, last_category_search, first_choice, second_choice, empty_search
+    # global df, df_keyword, last_category_search, first_choice, second_choice, empty_search
+    global user_states
+    user_id = event.source.user_id
     input_text = str(event.message.text)
-    if not input_text.isnumeric():
-        last_category_search = event.message.text
-        df_keyword = df.query(f'categoryName.str.contains("{event.message.text}")', engine='python')
-        print('query result', df_keyword)
-        df_keyword.drop_duplicates('categoryName', inplace=True)
-        delete_row = df_keyword[df_keyword["categoryName"]==input_text].index
-        df_keyword = df_keyword.drop(delete_row).iloc[:5]
-
-    myArr2 = []
-    textArr = []
-    if df_keyword.empty:
-        textArr.append('Sorry, no recipe found!')
-        first_choice = False
-        second_choice = False
-        empty_search = True
+    if user_id not in user_states:
+        user_states[user_id] = {
+            'df': pd.DataFrame(columns=['category1','category2','category3','categoryId','categoryName']),
+            'df_keyword': pd.DataFrame(),
+            'last_category_search': input_text,
+            #'first_choice': False,
+            'category_selected': False,
+            #'empty_search': False
+        }
     else:
-        first_choice = not second_choice
-    for i, category in enumerate(df_keyword["categoryName"], 1):
-        #print('@E NUMERO??', input_text.isnumeric())
-        if i==1:
-            textArr.append("Choose a recipe category:")
-        textArr.append(f"{i} - {category}")
-
-    if first_choice|empty_search:
-        print('Make user choose category')
-        messages = TextMessage(text='\n\n'.join(textArr))
-        line_bot_api.reply_message(event.reply_token, messages=[TextMessage(text='\n\n'.join(textArr))] )
-        if empty_search:
-            first_choice = True
-            second_choice = False
-            empty_search = False
-        else:
-            first_choice = False
-            second_choice = True
-
-
-
-    elif second_choice:
+        user_states[user_id]['last_category_search'] = input_text
+    df_keyword = user_states[user_id]['df_keyword']
+    last_category_search = user_states[user_id]['last_category_search']
+    #first_choice = user_states[user_id]['first_choice']
+    category_selected = user_states[user_id]['category_selected']
+    #empty_search = user_states[user_id]['empty_search']
+    textArr = []
+    print('last_category_search:', last_category_search, category_selected)
+    if last_category_search.isnumeric() and category_selected:
+        print('\nEntrou category_selected')
         category_ranking = requests.get(f'https://app.rakuten.co.jp/services/api/Recipe/CategoryRanking/20170426?applicationId=1043289948570835263&categoryId={df_keyword["categoryId"].values[int(input_text)-1]}')
         json_data_ranking = json.loads(category_ranking.text)
         myDBB = pd.DataFrame(columns=['foodImageUrl','mediumImageUrl','nickname','recipeCost', 'recipeDescription', 'rank', 'smallImageUrl', 'recipeUrl', 'recipeTitle'])
         for detail in json_data_ranking['result']:
-            #df = df._append({'category1':category['categoryId'],'category2':"",'category3':"",'categoryId':category['categoryId'],'categoryName':category['categoryName']}, ignore_index=True)
             myDBB = myDBB._append({'foodImageUrl': detail['foodImageUrl'], 'mediumImageUrl': detail['mediumImageUrl'], 'nickname': detail['nickname'], 'recipeCost': detail['recipeCost'], 'recipeDescription': detail['recipeDescription'], 'rank': detail['rank'], 'smallImageUrl': detail['smallImageUrl'], 'recipeUrl': detail['recipeUrl'], 'recipeTitle': detail['recipeTitle']}, ignore_index=True)
-        notes2 = []
+        carousel_items = []
         for i, recipe in enumerate(myDBB["recipeUrl"], 1):
-            column = {
+            carousel_column = {
                 "thumbnail_image_url": f"{myDBB['foodImageUrl'][i-1]}",
                 "title": f"{myDBB['recipeTitle'][i-1]}",
                 "text": f"{myDBB['recipeDescription'][i-1][:59]}",  # Corrected the syntax here
@@ -121,36 +117,37 @@ def response_message(event):
                     {"type": "uri", "label": "レシピURLへ", "uri": f"{myDBB['recipeUrl'][i-1]}"}
                 ]
             }
-            notes2.append(column)
-        notes = [CarouselColumn(thumbnail_image_url=myDBB['foodImageUrl'][0],
-                                title=myDBB['recipeTitle'][0],
-                                text=myDBB['recipeDescription'][0][:59],
-                                actions=[{"type": "message","label": "レシピURLへ","text": f"{myDBB['recipeUrl'][0]}"}]),
+            carousel_items.append(carousel_column)
 
-                CarouselColumn(thumbnail_image_url=myDBB['foodImageUrl'][1],
-                                title=myDBB['recipeTitle'][1],
-                                text=myDBB['recipeDescription'][1][:59],
-                                actions=[
-                                    {"type": "message", "label": "レシピURLへ", "text": f"{myDBB['recipeUrl'][1]}"}]),
-
-                CarouselColumn(thumbnail_image_url=myDBB['foodImageUrl'][2],
-                                title=myDBB['recipeTitle'][2],
-                                text=myDBB['recipeDescription'][2][:59],
-                                actions=[
-                                    {"type": "message", "label": "レシピURLへ", "text": f"{myDBB['recipeUrl'][2]}"}]),
-                CarouselColumn(thumbnail_image_url=myDBB['foodImageUrl'][2],
-                                title=myDBB['recipeTitle'][2],
-                                text=myDBB['recipeDescription'][2][:59],
-                                actions=[
-                                    {"type": "message", "label": "レシピURLへ", "text": f"{myDBB['recipeUrl'][2]}"}])]
         messages = TemplateSendMessage(
             alt_text='template',
-            template=CarouselTemplate(columns=notes2),
+            template=CarouselTemplate(columns=carousel_items),
         )
 
         line_bot_api.reply_message(event.reply_token, messages=messages)
-        second_choice = False
+        user_states[user_id]['category_selected'] = False
+    else:
+        print('searching category\n')
+        df_keyword = df.query(f'categoryName.str.contains("{event.message.text}")', engine='python').copy()
+        print('query result\n', df_keyword)
+        df_keyword.drop_duplicates('categoryName', inplace=True)
+        delete_row = df_keyword[df_keyword["categoryName"]==input_text].index
+        df_keyword = df_keyword.drop(delete_row).iloc[:5]
+        user_states[user_id]['df_keyword'] = df_keyword
+        if df_keyword.empty:
+            print('entrou empty\n')
+            textArr.append('レシピは見つかりませんでした！')
+            user_states[user_id]['category_selected'] = False
+        else:
+            print('Make user choose category')
+            for i, category in enumerate(df_keyword["categoryName"], 1):
+                if i==1:
+                    textArr.append("レシピのカテゴリー（番号）を選択してください：")
+                textArr.append(f"{i} - {category}")
+            user_states[user_id]['category_selected'] = True
+        line_bot_api.reply_message(event.reply_token, messages=[TextMessage(text='\n\n'.join(textArr))] )
 
 if __name__ == "__main__":
+    warnings.filterwarnings("ignore", category=LineBotSdkDeprecatedIn30)
     port = int(os.getenv("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
